66ce7732b411da0f7715f2e35bbd5c636e497db5
bala-amypo <bala@amypo.in>
2025-12-21 13:56:25 +0000

    User commit

--- src/main/java/com/example/demo/repository/HoldingRecordRepository.java
+++ src/main/java/com/example/demo/repository/HoldingRecordRepository.java
@@ -1,12 +1,10 @@
 package com.example.demo.repository;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.HoldingRecord;
- import java.util.Long;
 
 @Repository
-public interface HoldingRecordRepository extends JpaRepository<HoldingRecord, Long>{
-
-
+public interface HoldingRecordRepository extends JpaRepository<HoldingRecord, Long> {
 }
--- src/main/java/com/example/demo/repository/InvestorProfileRepository.java
+++ src/main/java/com/example/demo/repository/InvestorProfileRepository.java
@@ -1,12 +1,10 @@
 package com.example.demo.repository;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.InvestorProfile;
- import java.util.Long;
 
 @Repository
-public interface InvestorProfileRepository extends JpaRepository<InvestorProfile, Long>{
-
-
+public interface InvestorProfileRepository extends JpaRepository<InvestorProfile, Long> {
 }
