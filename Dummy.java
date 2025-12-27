df613180ec9b47f7c5ffc9de352e21978f2f5ece
bala-amypo <bala@amypo.in>
2025-12-27 05:01:41 +0000

    User commit

---
+++ Dummy.java
@@ -0,0 +1,13 @@
+package sbs.rosedev.springFirst;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class Dummy {
+    @GetMapping("/show")
+    public String display() {
+        return "dummy dummy dummmmyyy";
+    }
+}
